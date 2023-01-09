61922f2fc9d926e9d090e347c2205855c6b67c42
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2023-01-09 23:11:54 +0900

    Create 2751.py

---
+++ 정렬/2751.py
@@ -0,0 +1,14 @@
+import sys
+
+#input()보다 readline()이 더 빠르다
+
+N = int(sys.stdin.readline())
+arr = []
+
+for i in range(N):
+  arr.append(int(sys.stdin.readline()))
+
+sorted_arr = sorted(arr)
+
+for j in range(N):
+  print(sorted_arr[j])
